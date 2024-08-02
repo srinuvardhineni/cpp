eecd358bd582c94bcf4e933916edc3836e81351f
Srinivasulu Vardhineni <srinivasulu.vardhineni@gmail.com>
2024-08-02 17:58:15 +0530

    my first commit

---
+++ cpp/basics_level_1/hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int user_age = 39;
+    double temperature = 20.6;
+    bool is_alive = false;
+    std::string user_name = "Srinu";
+
+    std::cout << user_age << std::endl;
+    std::cout << temperature << std::endl;
+    std::cout << is_alive << std::endl;
+    std::cout << user_name << std::endl;
+    return 0;
+}
