b6402177847e65bf62c3cc5633da5b345f2fd9eb
Srinivasulu Vardhineni <srinivasulu.vardhineni@gmail.com>
2024-08-03 11:32:44 +0530

    arrays with std functionality

--- cpp/basics_level_1/arrays.cpp
+++ cpp/basics_level_1/arrays.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
@@ -7,5 +8,13 @@ int main()
     std::cout << temperature_list[3] << std::endl;
     temperature_list[3] = 15.4;
     std::cout << temperature_list[3] << std::endl;
+
+    std::vector<double> temperatures = {34.5, 27.8, 26.8};
+    std::cout << temperatures.at(0) << std::endl;
+    temperatures.at(1) = 30.7;
+    std::cout << temperatures.at(1) << std::endl;
+    temperatures.push_back(17.4);
+    std::cout << temperatures.size() << std::endl;
+
     return 0;
 }
